a5674d205f06e5b383070755f0f5ddc36a11ff8e
satyam gaur <117989318+Satyamgaur1711@users.noreply.github.com>
2026-09-18 22:08:58 +0530

    Add files via upload

---
+++ Numpy/08_numpy_arry.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+a = np.arange(2, 100, 3)
+print(a)
+# 2 start from 2 and go till 100 with the jump of 3..
+
+x = np.linspace(10, 80, num=25)
+print(x)
+
+ar = np.array([15,16,14,95,25,36,85,74,5245,111])
+
+np.short(ar)
+print(ar)
